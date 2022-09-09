349c5cdc8aeae25160b99fd97793eda247d72d35
SrinivasNagaram <creator.srinivas@gmail.com>
2022-09-09 14:18:03 +0530

    Completed Project

---
+++ src/utils/constants.js
@@ -0,0 +1,27 @@
+import CodeIcon from "@mui/icons-material/Code";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "JavaScript", icon: <CodeIcon /> },
+  { name: "ES6", icon: <CodeIcon /> },
+  { name: "ReactJS", icon: <CodeIcon /> },
+  { name: "Redux", icon: <CodeIcon /> },
+  { name: "NextJS", icon: <CodeIcon /> },
+  { name: "GraphQL", icon: <CodeIcon /> },
+  { name: "NodeJS", icon: <CodeIcon /> },
+  { name: "ExpressJS", icon: <CodeIcon /> },
+  { name: "HTML5", icon: <CodeIcon /> },
+  { name: "CSS3", icon: <CodeIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+];
+
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const demoChannelUrl = "channel/UCr01r4xCBgSX2X0MRGomnIQ";
+export const demoVideoUrl = "/video/Ke90Tje7VS0";
+export const demoChannelTitle = "ReactJS";
+export const demoVideoTitle = "React JS - React Tutorial for Beginners";
+export const demoProfilePicture =
+  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
